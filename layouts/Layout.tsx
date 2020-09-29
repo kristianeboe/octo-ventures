a805421e6fbfc5673c823df5bd3580d1c258a81e
Kenneth Westli <kenneth.westli@soprasteria.com>
2020-09-29 19:53:55 +0200

    Design improvements

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -36,50 +36,48 @@ export const Layout: React.FC = ({children}) => {
     return (
         <CompanyMetricContext.Provider
             value={{chanceOfSuccess, computeChanceOfSuccess, companyEvaluation, computeCompanyEvaluation}}>
-            <div className={"app"}>
-                <div className={"header"}>
-                    <div className={"logo"}>
-                        <PerigeeLogo/>
-                    </div>
-                    <div className={"userMenu"}>
-                        <h3>Welcome back{user && `, ${user.name}`}!</h3>
-                    </div>
+            <div className={"header"}>
+                <div className={"logo"}>
+                    <PerigeeLogo/>
                 </div>
-                <div className={"container"}>
-                    {/*<div className={"headerMenu"}>*/}
-                    {/*    <div className={"companyName"}><h1>{companyName}</h1></div>*/}
-                    {/*    {user && <div className={"companyName"}><h2>Welcome back, {user.name}</h2></div>}*/}
-                    {/*    <div className={"companyMetrics"}>*/}
-                    {/*        <div className={"companyEvaluation"}>Company valuation: {companyEvaluation} NOK</div>*/}
-                    {/*        <div className={"companySuccessRate"}>Your chance of success: {chanceOfSuccess} %</div>*/}
-                    {/*    </div>*/}
-                    {/*    <br/>*/}
-                    {/*    <div className={"phases"}>*/}
-                    {/*        <Link href={"/phase/problem"}>*/}
-                    {/*            <div className={"phase"}>PROBLEM</div>*/}
-                    {/*        </Link>*/}
-                    {/*        <Link href={"/phase/team"}>*/}
-                    {/*            <div className={"phase"}>TEAM</div>*/}
-                    {/*        </Link>*/}
-                    {/*        <Link href={"/phase/research"}>*/}
-                    {/*            <div className={"phase"}>RESEARCH</div>*/}
-                    {/*        </Link>*/}
-                    {/*        <Link href={"/phase/develop"}>*/}
-                    {/*            <div className={"phase"}>DEVELOP</div>*/}
-                    {/*        </Link>*/}
-                    {/*        <Link href={"/phase/launch"}>*/}
-                    {/*            <div className={"phase"}>LAUNCH</div>*/}
-                    {/*        </Link>*/}
-                    {/*        <Link href={"/phase/scale"}>*/}
-                    {/*            <div className={"phase"}>SCALE</div>*/}
-                    {/*        </Link>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-                    <br/>
-                    {
-                        children
-                    }
+                <div className={"userMenu"}>
+                    <h3>Welcome back{user && `, ${user.name}`}!</h3>
                 </div>
+            </div>
+            <div className={"container"}>
+                {/*<div className={"headerMenu"}>*/}
+                {/*    <div className={"companyName"}><h1>{companyName}</h1></div>*/}
+                {/*    {user && <div className={"companyName"}><h2>Welcome back, {user.name}</h2></div>}*/}
+                {/*    <div className={"companyMetrics"}>*/}
+                {/*        <div className={"companyEvaluation"}>Company valuation: {companyEvaluation} NOK</div>*/}
+                {/*        <div className={"companySuccessRate"}>Your chance of success: {chanceOfSuccess} %</div>*/}
+                {/*    </div>*/}
+                {/*    <br/>*/}
+                {/*    <div className={"phases"}>*/}
+                {/*        <Link href={"/phase/problem"}>*/}
+                {/*            <div className={"phase"}>PROBLEM</div>*/}
+                {/*        </Link>*/}
+                {/*        <Link href={"/phase/team"}>*/}
+                {/*            <div className={"phase"}>TEAM</div>*/}
+                {/*        </Link>*/}
+                {/*        <Link href={"/phase/research"}>*/}
+                {/*            <div className={"phase"}>RESEARCH</div>*/}
+                {/*        </Link>*/}
+                {/*        <Link href={"/phase/develop"}>*/}
+                {/*            <div className={"phase"}>DEVELOP</div>*/}
+                {/*        </Link>*/}
+                {/*        <Link href={"/phase/launch"}>*/}
+                {/*            <div className={"phase"}>LAUNCH</div>*/}
+                {/*        </Link>*/}
+                {/*        <Link href={"/phase/scale"}>*/}
+                {/*            <div className={"phase"}>SCALE</div>*/}
+                {/*        </Link>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
+                <br/>
+                {
+                    children
+                }
             </div>
         </CompanyMetricContext.Provider>
 
